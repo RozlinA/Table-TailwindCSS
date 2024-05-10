36757b526f3d745f2d5af00d42435d39113f49c8
Rözlin Akkurt <rozlin.akkurt@hotmail.com>
2024-05-10 15:42:12 +0200

    Dark mode function and styling

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 import './style.css'
+
+const darkModeToggle = document.getElementById('darkModeToggle');
+
+darkModeToggle.addEventListener('click', () => {
+  document.documentElement.classList.toggle('dark');
+  if (document.documentElement.classList.contains('dark')) {
+    darkModeToggle.innerHTML = '<i class="bi bi-sun-fill"></i> Light Mode';
+  } else {
+    darkModeToggle.innerHTML = '<i class="bi bi-moon-stars-fill"></i> Dark Mode';
+  }
+});
